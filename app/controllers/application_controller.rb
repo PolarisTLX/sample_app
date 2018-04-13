85948d739567def33f883ff69319e523bf6640a4
PolarisTLX <paul.a.rail@gmail.com>
2018-04-13 12:03:04 -0400

    Added hello to have minimum to display on Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world! This is the minimum to get something showing on Heroku."
+  end
 end
